a8074e1b574cd2bf3d6d7f11c98a893cbac1ea31
phamtanphat <phatdroid94@gmail.com>
2021-09-20 21:18:01 +0700

    switch case

--- lib/buoi1.dart
+++ lib/buoi1.dart
@@ -52,10 +52,10 @@ void main() {
   // mutable vs immutable
 
   // 5 : Toán tử
-  int a = 5;
-  int b = 5;
-
-  int ketQua = a++ - --b + ++a - --a + a-- + b--;
+  // int a = 5;
+  // int b = 5;
+  //
+  // int ketQua = a++ - --b + ++a - --a + a-- + b--;
   // 5 - --b + ++a - --a + a-- + b--  ;a = 6  b = 5
   // 5 - 4 + ++a - --a + a-- + b--  ;a = 6  b = 4
   // 5 - 4 + 7 - --a + a-- + b--  ;a = 7  b = 4
@@ -63,9 +63,61 @@ void main() {
   // 5 - 4 + 7 - 6 + 6 + 4  ;a = 5  b = 3
   // 5 - 4 + 7 - 6 + 6 + 4  ;a = 5  b = 3
   // 12 , a = 5 , b= 3
-  print(ketQua);
-  print(a);
-  print(b);
+  // print(ketQua);
+  // print(a);
+  // print(b);
 
+  // 6 : Câu điều kiện
+  // int teo = 5;
+  // int ty = 10;
+  //
+  // if (teo > ty){
+  //   print("Tèo lớn hơn Tý");
+  // }else if (teo == ty){
+  //   print("Tèo bằng tuổi với Tý");
+  // }else{
+  //   print("Tèo nhỏ hơn Tý");
+  // }
 
+  // int a = null;
+  // a ??=0;
+  //
+  // int ketQua = a + 5;
+  // print(ketQua);
+
+  // int a = 5;
+  //
+  // if (a is num){
+  //   print("A có kiểu number");
+  // }
+
+  // int a = 5;
+  // String b = "6";
+
+  // type casting
+
+  // 7 : switch case (Câu điều kiện)
+  int month = 8;
+  switch (month) {
+    case 1:
+    case 2:
+    case 3:
+      print("Quý 1");
+      break;
+    case 4:
+    case 5:
+    case 6:
+      print("Quý 2");
+      break;
+    case 7:
+    case 8:
+    case 9:
+      print("Quý 3");
+      break;
+    case 10:
+    case 11:
+    case 12:
+      print("Quý 4");
+      break;
+  }
 }
